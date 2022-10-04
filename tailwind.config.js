d78b3d34215c8e32f29b61e2080bbb4122928d1e
Kartik4152 <gaming4152@gmail.com>
2022-10-04 21:51:13 +0530

    feat: Added color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-400": "#240D57",
+        "primary-300": "#501FC1",
+        "primary-200": "#8456EC",
+        "primary-100": "#E87BF8",
+        "secondary-300":"#CCB6FF",
+        "secondary-200":"#EDE5FF",
+        "secondary-100":"#EDE5FF",
+        "alert-error-light": "#FFD7E0",
+        "alert-error": "#E61445",
+        "alert-success-light": "#D3FFE2",
+        "alert-success": "#00805E",
+        "neutral-600": "#4F4F4F",
+        "neutral-500": "#828282",
+        "neutral-400": "#BDBDBD",
+        "neutral-300": "#E0E0E0",
+        "neutral-200": "#F2F2F2",
+        "neutral-100": "#FBFAFF"
+      }
+    },
   },
   plugins: [],
 }
