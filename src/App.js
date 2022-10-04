11619354e27c092f0305a845f45430bec7bf0822
Kartik4152 <gaming4152@gmail.com>
2022-10-04 22:06:34 +0530

    feat: Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import LandingPage from "./pages";
+import CreatePage from "./pages/create";
+import EventPage  from "./pages/event";
+
 import './styles/global.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/create",
+    element: <CreatePage />,
+  },
+  {
+    path: "/event",
+    element: <EventPage />,
+  }
+])
+
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
